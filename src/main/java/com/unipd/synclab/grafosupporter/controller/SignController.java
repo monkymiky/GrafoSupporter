512d56d39805160f52f797ebbfe1371c012f7b27
monkymiky <michines2001@gmail.com>
2025-05-21 10:03:29 +0200

    SignController get

---
+++ src/main/java/com/unipd/synclab/grafosupporter/controller/SignController.java
@@ -0,0 +1,26 @@
+package com.unipd.synclab.grafosupporter.controller;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.unipd.synclab.grafosupporter.model.Sign;
+import com.unipd.synclab.grafosupporter.service.SignService;
+
+@RestController
+@RequestMapping("/signs") 
+public class SignController {
+    private final SignService signService;
+
+    public SignController(SignService signService){
+        this.signService = signService;
+    }
+
+    @GetMapping
+    public List<Sign> getMethodName() {
+        return signService.getAllSign();
+    }
+}
