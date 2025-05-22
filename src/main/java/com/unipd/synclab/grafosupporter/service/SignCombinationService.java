d0bf921a24e7fc6a0b9c5ba753ed611e723bdc65
monkymiky <michines2001@gmail.com>
2025-05-22 14:58:18 +0200

    SignCombinationService with add edit delete

--- src/main/java/com/unipd/synclab/grafosupporter/service/SignCombinationService.java
+++ src/main/java/com/unipd/synclab/grafosupporter/service/SignCombinationService.java
@@ -4,6 +4,7 @@ import java.security.InvalidParameterException;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,8 @@ import com.unipd.synclab.grafosupporter.model.SignCombination;
 import com.unipd.synclab.grafosupporter.repository.SignCombinationRepository;
 import com.unipd.synclab.grafosupporter.repository.SignRepository;
 
+import jakarta.persistence.EntityNotFoundException;
+
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
@@ -21,6 +24,15 @@ public class SignCombinationService {
     SignRepository signRepository;
     @Autowired
     SignCombinationRepository signCombinationRepository;
+
+    @Transactional(readOnly = true)
+    public SignCombination getSignCombinationsById(Long id) {
+        Optional<SignCombination> signCombination = signCombinationRepository.findById(id);
+        if (signCombination.isPresent())
+            return signCombination.get();
+        else
+            throw new InvalidParameterException("signCombination with id = " + id + " doesnt'exist.");
+    }
 
     @Transactional(readOnly = true)
     public List<SignCombination> getSignCombinationsWithoutValue(Map<Long, Integer> serchedSign) {
@@ -39,6 +51,32 @@ public class SignCombinationService {
                         )).collect(Collectors.toList());
 
     };
+
+    @Transactional
+    public void addSignCombination(SignCombination signCombination) {
+        signCombinationRepository.save(signCombination);
+    }
+
+    @Transactional
+    public void editSignCombination(SignCombination signCombination) {
+        SignCombination signCombinationToEdit = signCombinationRepository.findById(signCombination.getId())
+                .orElseThrow(() -> new EntityNotFoundException(
+                        "Impossible to edit - SignCombination not found with id: " + signCombination.getId()));
+        signCombinationToEdit.setLongDescription((signCombination.getLongDescription()));
+        signCombinationToEdit.setShortDescription(signCombination.getShortDescription());
+        signCombinationToEdit.setSigns(signCombination.getSigns());
+        signCombinationToEdit.setSourceBook(signCombination.getSourceBook());
+        signCombinationToEdit.setTitle(signCombination.getTitle());
+        signCombinationToEdit.setSourceBook(signCombination.getSourceBook());
+    }
+
+    @Transactional
+    public void deleteSignCombination(Long id) {
+        if (!signCombinationRepository.existsById(id)) {
+            throw new EntityNotFoundException("SignCombination not found with id: " + id);
+        }
+        signCombinationRepository.deleteById(id);
+    }
 
     private boolean matchValues(Integer frontendValue, Integer backendValue) {
         switch (frontendValue) {
