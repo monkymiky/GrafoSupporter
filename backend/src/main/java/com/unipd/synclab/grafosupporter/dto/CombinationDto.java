577a49c683f1786085067446d90a79733d532c66
monkymiky <michines2001@gmail.com>
2025-06-20 16:02:42 +0200

    implementato trim e validazione notempty

--- backend/src/main/java/com/unipd/synclab/grafosupporter/dto/CombinationDto.java
+++ backend/src/main/java/com/unipd/synclab/grafosupporter/dto/CombinationDto.java
@@ -6,14 +6,24 @@ import lombok.NoArgsConstructor;
 
 import java.util.List;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.unipd.synclab.grafosupporter.utility.StringTrimDeserializer;
+
+import jakarta.validation.constraints.NotEmpty;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class CombinationDto {
 
     private Long id;
+    @NotEmpty
+    @JsonDeserialize(using = StringTrimDeserializer.class)
     private String title;
+    @NotEmpty
+    @JsonDeserialize(using = StringTrimDeserializer.class)
     private String shortDescription;
+    @JsonDeserialize(using = StringTrimDeserializer.class)
     private String longDescription;
     private String originalTextCondition;
     private String author;
