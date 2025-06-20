7f88122aeef57ec110db901e35eae84f7472398b
monkymiky <michines2001@gmail.com>
2025-06-20 17:24:22 +0200

    inizializzazione homepage e navbar

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,19 +1,25 @@
 import { Routes } from '@angular/router';
+import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/ricerca-combinazioni', pathMatch: 'full' },
   {
-    path: 'ricerca-combinazioni',
-    loadComponent: () =>
-      import(
-        './search-combinations/search-combinations-page/search-combinations-page.component'
-      ).then((m) => m.SearchCombinationsPageComponent),
-  },
-  {
-    path: 'aggiunta-combinazioni',
-    loadComponent: () =>
-      import('./add-combinations/add-combinations.component').then(
-        (m) => m.AddCombinationsComponent
-      ),
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      {
+        path: '',
+        loadComponent: () =>
+          import('./pages/home/home.component').then((m) => m.HomeComponent),
+        title: 'Homepage - Grafologo Digitale',
+      },
+      {
+        path: 'ricerca-combinazioni',
+        loadComponent: () =>
+          import(
+            './search-combinations/search-combinations-page/search-combinations-page.component'
+          ).then((m) => m.SearchCombinationsPageComponent),
+        title: 'Ricerca Combinazioni',
+      },
+    ],
   },
 ];
